a55edb3215159274cb347d04c273e8082e86e5f0
hgg <hgg93@icloud.com>
2016-01-03 21:01:45 +0800

    初始化项目

---
+++ GG/GG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GG
+//
+//  Created by DSB on 16/1/3.
+//  Copyright © 2016年 DSB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GG/GG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GG
+//
+//  Created by DSB on 16/1/3.
+//  Copyright © 2016年 DSB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
